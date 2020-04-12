260a91c040f055acaac952b07cd8a109668422ba
maxwlang <maxwlang@gmail.com>
2020-04-11 20:53:34 -0500

    Swap secret out for generator.

--- config/session.js
+++ config/session.js
@@ -6,7 +6,7 @@ const session = {
 
     // The session secret
     // If set to %VBASE_UNIQUE% in single quotes, VBase will generate a unique key on first run.
-    secret: '40068a6d-cc0e-4d46-826d-85b5738a2677',
+    secret: '%VBASE_UNIQUE%',
 };
 
 module.exports = session;
